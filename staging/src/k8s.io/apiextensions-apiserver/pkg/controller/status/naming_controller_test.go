kubernetes/apiextensions-apiserver

client-go/tools/cache: add APIs with context parameter
The context is used for cancellation and to support contextual logging.

In most cases, alternative *WithContext APIs get added, except for
NewIntegerResourceVersionMutationCache where code searches indicate that the
API is not used downstream.

An API break around SharedInformer couldn't be avoided because the
alternative (keeping the interface unchanged and adding a second one with
the new method) would have been worse. controller-runtime needs to be updated
because it implements that interface in a test package. Downstream consumers of
controller-runtime will work unless they use those test package.

Converting Kubernetes to use the other new alternatives will follow. In the
meantime, usage of the new alternatives cannot be enforced via logcheck
yet (see https://github.com/kubernetes/kubernetes/issues/126379 for the
process).

Passing context through and checking it for cancellation is tricky for event
handlers. A better approach is to map the context cancellation to the normal
removal of an event handler via a helper goroutine. Thanks to the new
HandleErrorWithLogr and HandleCrashWithLogr, remembering the logger is
sufficient for handling problems at runtime.

Kubernetes-commit: 4638ba971661497b147906b8977ae206c9dd6e44

pkg/controller/status/naming_controller_test.go
@@ -27,6 +27,7 @@ import (
 	listers "k8s.io/apiextensions-apiserver/pkg/client/listers/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/klog/v2/ktesting"
 )
 
 type crdBuilder struct {
@@ -321,25 +322,30 @@ func TestSync(t *testing.T) {
 	}
 
 	for _, tc := range tests {
-		crdIndexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
-		for _, obj := range tc.existing {
-			crdIndexer.Add(obj)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			logger, _ := ktesting.NewTestContext(t)
+			crdIndexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
+			for _, obj := range tc.existing {
+				if err := crdIndexer.Add(obj); err != nil {
+					t.Fatalf("unexpected error: %v", err)
+				}
+			}
 
-		c := NamingConditionController{
-			crdLister:        listers.NewCustomResourceDefinitionLister(crdIndexer),
-			crdMutationCache: cache.NewIntegerResourceVersionMutationCache(crdIndexer, crdIndexer, 60*time.Second, false),
-		}
-		actualNames, actualNameConflictCondition, establishedCondition := c.calculateNamesAndConditions(tc.in)
+			c := NamingConditionController{
+				crdLister:        listers.NewCustomResourceDefinitionLister(crdIndexer),
+				crdMutationCache: cache.NewIntegerResourceVersionMutationCache(logger, crdIndexer, crdIndexer, 60*time.Second, false),
+			}
+			actualNames, actualNameConflictCondition, establishedCondition := c.calculateNamesAndConditions(tc.in)
 
-		if e, a := tc.expectedNames, actualNames; !reflect.DeepEqual(e, a) {
-			t.Errorf("%v expected %v, got %#v", tc.name, e, a)
-		}
-		if e, a := tc.expectedNameConflictCondition, actualNameConflictCondition; !apiextensionshelpers.IsCRDConditionEquivalent(&e, &a) {
-			t.Errorf("%v expected %v, got %v", tc.name, e, a)
-		}
-		if e, a := tc.expectedEstablishedCondition, establishedCondition; !apiextensionshelpers.IsCRDConditionEquivalent(&e, &a) {
-			t.Errorf("%v expected %v, got %v", tc.name, e, a)
-		}
+			if e, a := tc.expectedNames, actualNames; !reflect.DeepEqual(e, a) {
+				t.Errorf("expected %v, got %#v", e, a)
+			}
+			if e, a := tc.expectedNameConflictCondition, actualNameConflictCondition; !apiextensionshelpers.IsCRDConditionEquivalent(&e, &a) {
+				t.Errorf("expected %v, got %v", e, a)
+			}
+			if e, a := tc.expectedEstablishedCondition, establishedCondition; !apiextensionshelpers.IsCRDConditionEquivalent(&e, &a) {
+				t.Errorf("expected %v, got %v", e, a)
+			}
+		})
 	}
 }

vsreedharv/kubernetes

staging/src/k8s.io/apiextensions-apiserver/pkg/controller/status/naming_controller_test.go
@@ -27,6 +27,7 @@ import (
 	listers "k8s.io/apiextensions-apiserver/pkg/client/listers/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/klog/v2/ktesting"
 )
 
 type crdBuilder struct {
@@ -321,25 +322,30 @@ func TestSync(t *testing.T) {
 	}
 
 	for _, tc := range tests {
-		crdIndexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
-		for _, obj := range tc.existing {
-			crdIndexer.Add(obj)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			_, ctx := ktesting.NewTestContext(t)
+			crdIndexer := cache.NewIndexer(cache.MetaNamespaceKeyFunc, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
+			for _, obj := range tc.existing {
+				if err := crdIndexer.Add(obj); err != nil {
+					t.Fatalf("unexpected error: %v", err)
+				}
+			}
 
-		c := NamingConditionController{
-			crdLister:        listers.NewCustomResourceDefinitionLister(crdIndexer),
-			crdMutationCache: cache.NewIntegerResourceVersionMutationCache(crdIndexer, crdIndexer, 60*time.Second, false),
-		}
-		actualNames, actualNameConflictCondition, establishedCondition := c.calculateNamesAndConditions(tc.in)
+			c := NamingConditionController{
+				crdLister:        listers.NewCustomResourceDefinitionLister(crdIndexer),
+				crdMutationCache: cache.NewIntegerResourceVersionMutationCache(ctx, crdIndexer, crdIndexer, 60*time.Second, false),
+			}
+			actualNames, actualNameConflictCondition, establishedCondition := c.calculateNamesAndConditions(tc.in)
 
-		if e, a := tc.expectedNames, actualNames; !reflect.DeepEqual(e, a) {
-			t.Errorf("%v expected %v, got %#v", tc.name, e, a)
-		}
-		if e, a := tc.expectedNameConflictCondition, actualNameConflictCondition; !apiextensionshelpers.IsCRDConditionEquivalent(&e, &a) {
-			t.Errorf("%v expected %v, got %v", tc.name, e, a)
-		}
-		if e, a := tc.expectedEstablishedCondition, establishedCondition; !apiextensionshelpers.IsCRDConditionEquivalent(&e, &a) {
-			t.Errorf("%v expected %v, got %v", tc.name, e, a)
-		}
+			if e, a := tc.expectedNames, actualNames; !reflect.DeepEqual(e, a) {
+				t.Errorf("expected %v, got %#v", e, a)
+			}
+			if e, a := tc.expectedNameConflictCondition, actualNameConflictCondition; !apiextensionshelpers.IsCRDConditionEquivalent(&e, &a) {
+				t.Errorf("expected %v, got %v", e, a)
+			}
+			if e, a := tc.expectedEstablishedCondition, establishedCondition; !apiextensionshelpers.IsCRDConditionEquivalent(&e, &a) {
+				t.Errorf("expected %v, got %v", e, a)
+			}
+		})
 	}
 }